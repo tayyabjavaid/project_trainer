6ac3b10a77bdadc32837eb2dfbe220f9f3ad8ed0
shoaib <shoaib.fst@hotmail.com>
2017-01-08 22:46:58 +0500

    Correcting average function

--- mpu_6050/mpu_6050.ino
+++ mpu_6050/mpu_6050.ino
@@ -50,5 +50,5 @@ void loop(){
 
 int averaged(int arr[])
 {
-  return (arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9])/4;
+  return (arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9])/10;
 }
